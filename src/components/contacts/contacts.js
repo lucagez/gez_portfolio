a22e5edcb1a466c21630fb6996d36d02a55a6291
lucagez <lucagesmundo@yahoo.it>
2019-01-09 09:00:45 +0100

    updated email

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -6,7 +6,7 @@ export default (props) => (
   <section id="contacts" className={`${style.container}`}>
     <div className={`${style.text}`}>
       <h1>📞 <span>Contacts</span></h1>
-      <p><strong>email:</strong> <a rel="noopener noreferrer" target="_blank" href="mailto:contact@lucagez.net">contact@lucagez.net</a></p>
+      <p><strong>email:</strong> <a rel="noopener noreferrer" target="_blank" href="mailto:lucagesmundo@yahoo.it">lucagesmundo@yahoo.it</a></p>
       <p><strong>github:</strong> <a rel="noopener noreferrer" target="_blank" href="https://github.com/lucagez">github.com/lucagez</a></p>
       <p><strong>codepen:</strong> <a rel="noopener noreferrer" target="_blank" href="https://codepen.io/lucagez/">codepen.io/lucagez</a></p>
       <p><strong>facebook:</strong> <a rel="noopener noreferrer" target="_blank" href="https://facebook.com/luca.ges">fb.com/luca.ges</a></p>
